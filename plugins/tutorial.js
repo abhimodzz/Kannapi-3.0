29fab582becf5d97d700e198389989c20bfb9fd5
abhimodzz <78701752+abhimodzz@users.noreply.github.com>
2021-02-11 13:48:59 +0530

    Update tutorial.js

--- plugins/tutorial.js
+++ plugins/tutorial.js
@@ -1,36 +1,15 @@
 let handler  = async (m, { conn, usedPrefix: _p }) => {
   conn.reply(m.chat, `
 Tutorial BOT :
-Credit: *~[ᴍʀ.ʀᴀᴠᴀɴᴀɴ]*
-make your own what's app bot = https://youtube.com/c/MrRavanan
-Check all videos and learn
- what is termux : https://youtu.be/y1encEbGuAM
-*[TERMUX]*
-1) $pkg install git
-2) $pkg install ffmpeg
-3) $pkg install imagemagick
-4) $pkg install nodejs
-
-For the Github ..
-5) $git clone https://github.com/mrravanan/kannapi-2.0.git
-6) $ls
-7) $cd kannapi-2.0
-
-the *last* ..
-Penginstalannya cuy :)
-8) npm i
-9) node index.js
-
-_Can you see the tutorial on_
-*Our Youtube:*
-https://youtube.com/c/MrRavanan
-`.trim(), m)
+Credit: *~[ Abhinav ]*
+ 
+Sorry Currently Tutorial not Available
 }
 handler.help = ['tutorial']
 handler.tags = ['about']
 handler.command = /^(tutorial)$/i
 handler.owner = false
-handler.mods = false
+handler.mods = true
 handler.premium = false
 handler.group = false
 handler.private = false
